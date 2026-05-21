e2e1cd835aba5a0a7d5426f07a3938b6d0e3dcfa
wannabexaker <dimos.is.dev@gmail.com>
2026-05-21 17:18:19 +0300

    Update EM Spectrum favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,13 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
+  icons: {
+    icon: [
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/icon.svg', type: 'image/svg+xml' },
+    ],
+    apple: [{ url: '/apple-icon.png', sizes: '180x180', type: 'image/png' }],
+  },
 }
 
 export default function RootLayout({
